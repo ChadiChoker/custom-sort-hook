d3a002f38cbb1664f1fcfc6cab2b108395f44e61
chadichoker <chadichoker49@gmail.com>
2024-10-02 16:26:02 +0300

    first commit

---
+++ src/dummy-data.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: 'chadi',
+    fullName: 'Chadi Choker',
+    projects: [
+      { id: 'chadi_p1', title: 'Record the Vue course' },
+      { id: 'chadi_p2', title: 'Create more courses' },
+      { id: 'chadi_p3', title: 'Keep content updated' }
+    ]
+  },
+  {
+    id: 'm',
+    fullName: 'Mohammad Choker',
+    projects: [
+      { id: 'm_p1', title: 'Create more courses' },
+      { id: 'm_p2', title: 'Dive into data science topics' },
+      { id: 'm_p3', title: 'Various things' }
+    ]
+  },
+  {
+    id: 'julie',
+    fullName: 'Julie Jones',
+    projects: [
+      { id: 'julie_p1', title: 'Create agenda for next months' },
+      { id: 'julie_p2', title: 'Explore new topics' }
+    ]
+  },
+  {
+    id: 'michael',
+    fullName: 'Michael Miller',
+    projects: []
+  }
+];
